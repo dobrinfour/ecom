7ec8b6196c652ba953f3e5ce2ba4221f90f169eb
dobrinfour <t03do21@abdn.ac.uk>
2022-05-05 23:58:44 +0100

    added staticfiles

--- ecom/settings.py
+++ ecom/settings.py
@@ -133,8 +133,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
